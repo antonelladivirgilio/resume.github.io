5c965456b4b9b78e9a94a5b3af1ce297250db2f6
antonella <divirgilio.antonella@gmail.com>
2020-03-21 01:45:40 -0300

    add some styles, theme

--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,60 @@
 import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Baloo Chettan 2"].join(",")
+  },
   overrides: {
     MuiPaper: {
       root: {
         padding: "20px 30px 30px",
-        color: "#000",
+        color: "#212121",
         margin: "0 0 50px"
       }
     },
     MuiTypography: {
       root: {
         "&.MuiTypography-h1": {
-          color: "#bbb",
-          fontSize: "3rem"
+          color: "#212121",
+          fontSize: "3rem",
+          fontWeight: "500",
+          margin: "10px 0 40px"
         },
         "&.MuiTypography-h2": {
-          color: "#bbb",
+          color: "#757575",
           fontSize: "1.75rem"
+        },
+        "&.MuiTypography-body1": {
+          color: "#424242",
+          fontSize: "18px"
         }
       }
     },
     MuiButton: {
       root: {
-        borderRadius: "25px"
+        margin: "30px 0 10px",
+        borderRadius: "25px",
+        fontWeight: "bold",
+        "&.MuiButton-outlined": {
+          backgroundColor: "#fff",
+          color: "#212121",
+          borderColor: "#4db6ac"
+        },
+        "&.MuiButton-contained": {
+          backgroundColor: "#212121",
+          color: "#fff"
+        }
+      }
+    },
+    MuiDivider: {
+      root: {
+        margin: "25px 0 40px",
+        backgroundColor: "#9e9e9e"
+      }
+    },
+    MuiAvatar: {
+      root: {
+        margin: "10px 0 35px"
       }
     }
   }
