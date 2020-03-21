afd6f581f82e26b7d7ea1290925719b3f2223458
antonella <divirgilio.antonella@gmail.com>
2020-03-21 20:40:20 -0300

    add personal information

--- src/components/PersonalInformation/index.js
+++ src/components/PersonalInformation/index.js
@@ -4,7 +4,35 @@ import { Grid, Typography } from "@material-ui/core";
 function PersonalInformation() {
   return (
     <>
-      <Typography variant="h2">Personal information</Typography>
+      <Grid xs={12} item>
+        <Typography variant="h2">Personal information</Typography>
+      </Grid>
+      <Grid xs={12} item container alignItems="center">
+        <Grid xs={3} item className="text-bold-label">
+          FULL NAME
+        </Grid>
+        <Grid xs={9} item className="text-content">
+          Antonella Di Virgilio
+        </Grid>
+      </Grid>
+      <Grid xs={12} item container alignItems="center">
+        <Grid xs={3} item className="text-bold-label">
+          EMAIL
+        </Grid>
+        <Grid xs={9} item className="text-content">
+          divirgilioantonella@gmail.com
+        </Grid>
+      </Grid>
+      <style>{`
+        .text-content {
+          font-size: 17px;
+        }
+        
+        .text-bold-label {
+          font-weight: 600;
+          font-size: 15px;
+        }
+        `}</style>
     </>
   );
 }
