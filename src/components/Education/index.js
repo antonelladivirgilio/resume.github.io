bb8572c2a454b37e9904aff882ac185cfc35b15b
antonella <divirgilio.antonella@gmail.com>
2020-04-11 13:06:16 -0300

    add education and experience list

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -4,8 +4,28 @@ import { Grid, Typography } from "@material-ui/core";
 function Education() {
   return (
     <>
-      <Grid item container>
-        <Typography variant="h2">Education</Typography>
+      <Typography variant="h2">Education</Typography>
+      <Grid xs={10} item container className="text-name">
+        <ul>
+          <li>
+            <Typography className="text-bold-label">
+              Centro Universitario de idiomas (dec 2018 - now)
+            </Typography>
+            <Typography gutterBottom>English</Typography>
+          </li>
+          <li>
+            <Typography className="text-bold-label">
+              Educación IT (sep 2016)
+            </Typography>
+            <Typography gutterBottom>AngularJS</Typography>
+          </li>
+          <li>
+            <Typography className="text-bold-label">
+              UTN FRBA (aug 2016 - sep 2016)
+            </Typography>
+            <Typography gutterBottom>Advanced Front End Developer</Typography>
+          </li>
+        </ul>
       </Grid>
     </>
   );
