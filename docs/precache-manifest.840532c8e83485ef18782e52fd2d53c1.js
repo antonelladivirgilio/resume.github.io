8e3c6311dfbac176e6cd2ad18bfa13abd33f3983
antonella <divirgilio.antonella@gmail.com>
2020-05-02 19:03:14 -0300

    rename folder build to docs

---
+++ docs/precache-manifest.840532c8e83485ef18782e52fd2d53c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2aa39498cd53f8ec10f7b1847d3a45a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c591160319ca20100d03",
+    "url": "/static/css/main.06573f65.chunk.css"
+  },
+  {
+    "revision": "7adae1cfddf058fd3f30",
+    "url": "/static/js/2.8fc85e59.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.8fc85e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c591160319ca20100d03",
+    "url": "/static/js/main.0088b5e0.chunk.js"
+  },
+  {
+    "revision": "e45e20948a247110e75b",
+    "url": "/static/js/runtime-main.46b10d78.js"
+  },
+  {
+    "revision": "6f019b0d3bda5b76f2546e8e40515b48",
+    "url": "/static/media/avatar.6f019b0d.JPG"
+  },
+  {
+    "revision": "d8368736710d3662c9367e3b45ccea08",
+    "url": "/static/media/bg.d8368736.svg"
+  }
+]);
